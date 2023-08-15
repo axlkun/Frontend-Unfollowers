6fe81cd8f0127a6d5dfcd93e5b60367aca102d48
axlkun <axelcruz.dev@gmail.com>
2023-08-15 00:58:32 -0600

    adding validations

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="app">
-    <v-app-bar class="custom-border-bottom d-flex mb-6 bg-white">
+    <v-app-bar class="d-flex mb-6 bg-white" :elevation="0">
       <v-toolbar-title>Unfollowers</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn text>Tutorial</v-btn>
@@ -11,16 +11,56 @@
       <router-view></router-view>
     </main>
 
-    <footer></footer>
+    <v-footer
+    class="text-center d-flex flex-column" color="pink"
+  >
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        class="mx-4"
+        :icon="icon"
+        variant="text"
+      ></v-btn>
+    </div>
+
+    <!-- <div class="pt-0">
+      .
+    </div> -->
+
+    <v-divider></v-divider>
+
+    <div>
+      {{ new Date().getFullYear() }} — <strong>Unfollowers</strong>
+    </div>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-
+export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
 </script>
 
 <style>
 .custom-border-bottom {
   border-bottom: 1px solid gray; 
+}
+
+.section {
+  width: 95%;
+  max-width: 120rem;
+  margin: 100px auto;
+  min-height: 600px;
+  padding: 20px 10px;
+  border: 1px solid #E0E0E0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,27 @@
 
         <v-sheet class="d-flex flex-wrap justify-center">
             <v-sheet class="w-50">
-                <v-file-input :rules="rules" accept=".zip" label="Selecciona el archivo ZIP"></v-file-input>
+                <v-file-input :rules="rules" accept=".zip" label="Selecciona el archivo ZIP" @change="handleFileChange"
+                    @click:clear="clearFile">
+                </v-file-input>
             </v-sheet>
-            
+
             <v-sheet class="ma-5">
-            <v-btn prepend-icon="mdi mdi-account-remove" variant="tonal">
-                Buscar
-            </v-btn>
+                <v-btn prepend-icon="mdi mdi-account-remove" variant="tonal" @click="requestAPI">
+                    Buscar
+                </v-btn>
             </v-sheet>
         </v-sheet>
 
+        <v-snackbar v-model="alert" min-height="80px" transition="scroll-y-reverse-transition" >
+            {{ alertText }}
+
+            <template v-slot:actions>
+                <v-btn color="pink" variant="text" @click="alert = false">
+                    <span class="mdi mdi-close"></span>
+                </v-btn>
+            </template>
+        </v-snackbar>
     </div>
 </template>
 
@@ -21,20 +32,50 @@ export default {
     data: () => ({
         rules: [
             value => {
-                return !value || !value.length || value[0].size < 5000000 || 'El archivo ZIP debe pesar menos de 5MB!'
+                return !value || !value.length || value[0].size < 5000000 || 'El archivo ZIP debe pesar menos de 5MB'
             },
         ],
+        selectedFile: null,
+        alert: false,
+        alertText: 'No se ha seleccionado el archivo ZIP.'
     }),
+
+    methods: {
+
+        // Asignamos el ZIP recibido a la variable selectedFile
+        handleFileChange(event) {
+            const files = event.target.files;
+            if (files.length > 0) {
+                this.selectedFile = files[0];
+            } else {
+                this.clearFile();
+            }
+        },
+
+        // Realizamos la solicitud a la API 
+        requestAPI() {
+            if (this.selectedFile) {
+                console.log('Archivo seleccionado:', this.selectedFile);
+            } else {
+                this.showAlert();
+            }
+        },
+
+        // Limpiamos de memoria el archivo deseleccionado
+        clearFile() {
+            this.selectedFile = null;
+        },
+
+        showAlert() {
+            this.alert = true;
+            // setTimeout(() => {
+            //     this.alert = false;
+            // }, 5000);
+        }
+
+    }
 }
+
 </script>
 
-<style>
-.section {
-    width: 95%;
-    max-width: 120rem;
-    margin: 100px auto;
-    min-height: 600px;
-    padding: 20px 10px;
-    border: 1px solid #E0E0E0;
-}
-</style>
+<style></style>

cf45ebb5605eaf176b82670f2c8ac3aa5c23a9fc
axlkun <axelcruz.dev@gmail.com>
2023-11-29 17:44:18 -0600

    add blog view

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
+import Blog from './views/Blog.vue';
 import Results from './views/Results.vue';
 import Privacy from './views/Privacy.vue';
 import Terms from './views/Terms.vue';
@@ -8,6 +9,7 @@ import Cookies from './views/Cookies.vue';
 const routes = [
   { path: '/', component: Home },
   { path: '/results', component: Results },
+  { path: '/blog', component: Blog },
   { path: '/politica-de-privacidad-unfollowerstracker', component: Privacy },
   { path: '/terminos-y-condiciones-unfollowerstracker', component: Terms },
   { path: '/politica-de-cookies-unfollowerstracker', component: Cookies },

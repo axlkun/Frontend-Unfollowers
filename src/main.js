855a43e4cc368f5fbe1bed6b9fd32f815b993bf2
axlkun <axelcruz.dev@gmail.com>
2023-11-29 22:48:17 -0600

    add skeletonloader  component

--- src/main.js
+++ src/main.js
@@ -7,13 +7,17 @@ import router from './router.js';
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
+import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader';
 import * as directives from 'vuetify/directives'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css';
 import colors from 'vuetify/lib/util/colors'
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VSkeletonLoader,
+  },
   directives,
   colors
 })

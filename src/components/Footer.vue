27ba1847bd8ba3f2723f80250b6cce287ba9c8b2
axlkun <axelcruz.dev@gmail.com>
2023-11-30 11:44:13 -0600

    fix footer width

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,111 +1,104 @@
 <template>
-    <v-footer class="text-center d-flex flex-column" color="grey-lighten-2">
+  <v-footer class="text-center d-flex flex-column" color="grey-lighten-2">
 
-        <div class="d-flex flex-sm-row flex-column justify-space-evenly w-100 footer-section">
-  
-          <div class="section">
-            <p>Navegacion</p>
-  
-            <ul>
-              <li><a href="/">Inicio</a></li>
-              <li><a href="/#how-works">¿Cómo funciona?</a></li>
-              <li><a href="/#steps">Tutorial</a></li>
-              <li><a href="/#contact">Contacto</a></li>
-              <li><a href="/blog">Blog</a></li>
-            </ul>
-          </div>
-  
-          <div class="section">
-            <p>Herramientas Gratis</p>
-  
-            <ul>
-              <li><a href="/results">Quien no me sigue en Instagram</a></li>
-            </ul>
+    <div class="d-flex flex-sm-row flex-column justify-space-between footer-section">
 
-            <p class="ultimos-acrticulos">Últimos artículos</p>
-            <ul>
-              <li v-for="article in blogEntry"><a :href="'/blog/' + article.slug">{{article.title}}</a></li>
-          </ul>
-          </div>
-  
-          <div class="section">
-            <p>Más</p>
-  
-            <ul>
-              <li><a href="/terminos-y-condiciones-unfollowerstracker">Términos</a></li>
-              <li><a href="/politica-de-privacidad-unfollowerstracker">Privacidad</a></li>
-              <li><a href="/politica-de-cookies-unfollowerstracker">Cookies</a></li>
-            </ul>
-          </div>
-          
-        </div>
-        
-        <div class="d-flex flex-row justify-center w-100">
-          <p class="ma-1">
-            {{ new Date().getFullYear() }} — Unfollowers
-            <a class="reset-style" href="https://www.instagram.com/axlkun/" target="_blank">
-              By <strong>@axlkun</strong>&#129489;
-            </a>
-            Illustration by <a class="reset-style" href="https://freeicons.io/mobile-illustrations-2/dcbf-spotify-account-illustration-472" target="_blank">Athul Mc</a>
-            on <a class="reset-style" href="https://freeicons.io" target="_blank">freeicons.io</a>
-          </p>
-          
-        </div>
-        
-      </v-footer>
+      <div class="section">
+        <p>Navegacion</p>
+
+        <ul>
+          <li><a href="/">Inicio</a></li>
+          <li><a href="/#how-works">¿Cómo funciona?</a></li>
+          <li><a href="/#steps">Tutorial</a></li>
+          <li><a href="/#contact">Contacto</a></li>
+          <li><a href="/blog">Blog</a></li>
+        </ul>
+      </div>
+
+      <div class="section">
+        <p>Herramientas Gratis</p>
+
+        <ul>
+          <li><a href="/results">Quien no me sigue en Instagram</a></li>
+        </ul>
+
+      </div>
+
+      <div class="section">
+        <p class="ultimos-acrticulos">Últimos artículos</p>
+        <ul>
+          <li v-for="article in blogEntry"><a :href="'/blog/' + article.slug">{{ article.title }}</a></li>
+        </ul>
+      </div>
+
+      <div class="section">
+        <p>Más</p>
+
+        <ul>
+          <li><a href="/terminos-y-condiciones-unfollowerstracker">Términos</a></li>
+          <li><a href="/politica-de-privacidad-unfollowerstracker">Privacidad</a></li>
+          <li><a href="/politica-de-cookies-unfollowerstracker">Cookies</a></li>
+        </ul>
+      </div>
+
+    </div>
+
+    <div class="d-flex flex-row justify-center w-100">
+      <p class="ma-1">
+        {{ new Date().getFullYear() }} — Unfollowers
+        <a class="reset-style" href="https://www.instagram.com/axlkun/" target="_blank">
+          By <strong>@axlkun</strong>&#129489;
+        </a>
+        Illustration by <a class="reset-style"
+          href="https://freeicons.io/mobile-illustrations-2/dcbf-spotify-account-illustration-472" target="_blank">Athul
+          Mc</a>
+        on <a class="reset-style" href="https://freeicons.io" target="_blank">freeicons.io</a>
+      </p>
+
+    </div>
+
+  </v-footer>
 </template>
 
 <script>
 
 export default {
 
-    name: 'myFooter',
+  name: 'myFooter',
 
-    props: ['blogEntry'],
+  props: ['blogEntry'],
 
 }
 </script>
 
 <style scoped>
-.footer-section{
-    
-    width: 90%;
-    text-align: center;
-    margin: 0 auto;
-    border-bottom: 1px solid gray;
-  
-    @media only screen and (min-width: 960px) {
-        /* resto de vistas */
-        width: 100%;
-    }
-  }
-  
-  .footer-section{
-    line-height: 2;
-  }
-  
-  .footer-section li:hover{
+.footer-section {
+  width: 90%;
+  max-width: 120rem;
+  margin: 0 auto;
+  text-align: center;
+  border-bottom: 1px solid gray;
+}
+
+.footer-section {
+  line-height: 2;
+}
+
+.footer-section li:hover {
+  color: #E91E63;
+  cursor: pointer;
+}
+
+.footer-section p {
+  font-weight: 600;
+}
+
+.section a {
+  color: black;
+
+  &:hover {
     color: #E91E63;
-    cursor: pointer;
   }
-  
-  .footer-section p{
-    font-weight: 600;
-  }
-  
-  .section a{
-    color: black;
+}
 
-    &:hover{
-      color: #E91E63;
-    }
-  }
-
-  .ultimos-acrticulos{
-    margin-top: 35px;
-
-    @media only screen and (min-width: 960px) {
-      margin-top: 15px;
-  }
-  }
 </style>

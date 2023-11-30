0e94d633d8fd395bd9bd58da6bc6c5c979e5dafb
axlkun <axelcruz.dev@gmail.com>
2023-11-29 22:48:04 -0600

    add article view

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from './views/Home.vue';
 import Blog from './views/Blog.vue';
+import Article from './views/Article.vue';
 import Results from './views/Results.vue';
 import Privacy from './views/Privacy.vue';
 import Terms from './views/Terms.vue';
@@ -10,6 +11,12 @@ const routes = [
   { path: '/', component: Home },
   { path: '/results', component: Results },
   { path: '/blog', component: Blog },
+  { 
+    path: '/blog/:slug', 
+    name: 'articleDetail', 
+    component: Article, 
+    props: true
+  },
   { path: '/politica-de-privacidad-unfollowerstracker', component: Privacy },
   { path: '/terminos-y-condiciones-unfollowerstracker', component: Terms },
   { path: '/politica-de-cookies-unfollowerstracker', component: Cookies },

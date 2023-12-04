9049534be14fad4eedeff4ec67fda4da2fdb2197
axlkun <axelcruz.dev@gmail.com>
2023-12-04 17:53:35 -0600

    add cookies consent component

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,34 @@
       <router-view :blogEntry="blogEntry"></router-view>
     </main>
 
+    <div class="text-center">
+      <v-bottom-sheet v-if="!cookiesAccepted && currentRoute !== '/politica-de-cookies-unfollowerstracker'" v-model="sheet">
+        <v-card class="text-center">
+          <v-card-text>
+            <div>
+              Este sitio web utiliza cookies para asegurarnos que obtengas la mejor experiencia.
+            </div>
+          </v-card-text>
+          <v-card-actions class="mx-auto">
+            <v-btn
+              variant="text"
+              color="pink"
+              href="/politica-de-cookies-unfollowerstracker"
+            >
+              Leer más
+            </v-btn>
+            <v-btn
+              variant="tonal"
+              color="pink"
+              @click="acceptCookies"
+            >
+              Aceptar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-bottom-sheet>
+    </div>
+
     <myFooter :blogEntry="blogEntry"></myFooter>
   </v-app>
 </template>
@@ -26,7 +54,9 @@ export default {
   },
 
   data: () => ({
-    blogEntry: []
+    blogEntry: [],
+    sheet: true,
+    currentRoute: '',
   }),
 
   methods: {
@@ -38,11 +68,32 @@ export default {
         .catch(error => {
           console.error('Error al hacer la solicitud GET:', error);
         });
-      }
+    },
+
+    acceptCookies() {
+      // Al hacer clic en "Aceptar", actualiza el estado y almacena en localStorage
+      this.sheet = false;
+      localStorage.setItem('cookiesAccepted', 'true');
+    },
+  },
+
+  computed: {
+    cookiesAccepted() {
+      // Verifica si las cookies han sido aceptadas (almacenadas en localStorage)
+      return localStorage.getItem('cookiesAccepted') === 'true';
+    },
+  },
+
+  watch: {
+    $route(to, from) {
+      // Actualiza la propiedad currentRoute cuando cambia la ruta
+      this.currentRoute = to.path;
+    },
   },
 
   mounted() {
     this.getArticles();
+    this.currentRoute = this.$route.path;
   }
 }
 </script>
@@ -65,8 +116,8 @@ export default {
   margin: -15px auto;
 
   @media only screen and (min-width: 1024px) {
-      width: 100%;
-      
+    width: 100%;
+
   }
 }
 
@@ -78,29 +129,28 @@ export default {
 
 /* terminos y privacidad */
 
-.privacy h1{
+.privacy h1 {
   color: #E91E63;
 }
 
-.privacy{
-    
+.privacy {
+
   width: 90%;
   margin: 0 auto;
   line-height: 1.8;
 
   @media only screen and (min-width: 960px) {
-      /* resto de vistas */
-      max-width: 50%;
-      min-height: 100vh;
+    /* resto de vistas */
+    max-width: 50%;
+    min-height: 100vh;
   }
 }
 
-.section{
+.section {
   margin: 20px 0;
 }
 
-.fecha{
+.fecha {
   color: gray;
 }
-
 </style>
